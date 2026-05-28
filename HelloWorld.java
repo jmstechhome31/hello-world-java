985d4b79df4263e80140df9c0c58cf223e964e6f
jmstechhome31 <jmstechhome31@gmail.com>
2026-05-28 21:17:27 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,14 +3,6 @@ public class HelloWorld {
     System.out.println("Hello world!");
     System.out.println("Hello world!");
   System.out.println("Hello world! Devops");
-     System.out.println("Hello world! jenkins");
-     System.out.println("Hello world! Devops");
-     System.out.println("Hello world! jenkins");
-        System.out.println("Hello world!");
-    System.out.println("Hello world!");
-  System.out.println("Hello world! Devops");
-     System.out.println("Hello world! jenkins");
-     System.out.println("Hello world! Devops");
-     System.out.println("Hello world! jenkins");
+
   }
 }
